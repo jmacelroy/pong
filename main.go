2940476662438a29205966338bc7840264649dd3
Jacob MacElroy <jacob@okteto.com>
2022-08-26 19:40:24 -0600

    initial pong setup

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("starting pong server...")
+	http.HandleFunc("/", helloServer)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			log.Printf("%v: %v\n", name, h)
+		}
+	}
+	fmt.Fprint(w, "Hello planet earth!")
+}
